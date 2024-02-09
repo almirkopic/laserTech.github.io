80fba324e29345fef3b1eeedc7b9478c6bf233d8
Almir Kopic <133537874+almirkopic@users.noreply.github.com>
2024-02-09 22:18:42 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,64 +1,89 @@
 "use strict";
 // Slider top carousel
-let nextDom = document.getElementById("next");
-let prevDom = document.getElementById("prev");
-let carouselDom = document.querySelector(".carousel");
-let SliderDom = carouselDom.querySelector(".carousel .list");
-let thumbnailBorderDom = document.querySelector(".carousel .thumbnail");
-let thumbnailItemsDom = thumbnailBorderDom.querySelectorAll(".item");
-thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
-let timeRunning = 1500;
-let timeAutoNext = 7000;
-nextDom.onclick = function () {
-  showSlider("next");
+// Slider
+const slider = function () {
+  const slides = document.querySelectorAll(".home-slide"); // Change from ".slide" to ".home-slide"
+  const btnLeft = document.querySelector(".prev"); // Change from ".slider__btn--left" to ".prev"
+  const btnRight = document.querySelector(".next"); // Change from ".slider__btn--right" to ".next"
+  const dotContainer = document.querySelector(".dots");
+
+  let curSlide = 0;
+  const maxSlide = slides.length;
+
+  // Functions
+  const createDots = function () {
+    slides.forEach(function (_, i) {
+      dotContainer.insertAdjacentHTML(
+        "beforeend",
+        `<button class="dots__dot" data-slide="${i}"></button>`
+      );
+    });
+  };
+
+  const activateDot = function (slide) {
+    document
+      .querySelectorAll(".dots__dot")
+      .forEach((dot) => dot.classList.remove("dots__dot--active"));
+
+    document
+      .querySelector(`.dots__dot[data-slide="${slide}"]`)
+      .classList.add("dots__dot--active");
+  };
+
+  const goToSlide = function (slide) {
+    slides.forEach(
+      (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
+    );
+  };
+
+  // Next slide
+  const nextSlide = function () {
+    if (curSlide === maxSlide - 1) {
+      curSlide = 0;
+    } else {
+      curSlide++;
+    }
+
+    goToSlide(curSlide);
+    activateDot(curSlide);
+  };
+
+  const prevSlide = function () {
+    if (curSlide === 0) {
+      curSlide = maxSlide - 1;
+    } else {
+      curSlide--;
+    }
+    goToSlide(curSlide);
+    activateDot(curSlide);
+  };
+
+  const init = function () {
+    goToSlide(0);
+    createDots();
+
+    activateDot(0);
+  };
+  init();
+
+  // Event handlers
+  btnRight.addEventListener("click", nextSlide);
+  btnLeft.addEventListener("click", prevSlide);
+
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") prevSlide();
+    e.key === "ArrowRight" && nextSlide();
+  });
+
+  dotContainer.addEventListener("click", function (e) {
+    if (e.target.classList.contains("dots__dot")) {
+      const { slide } = e.target.dataset;
+      goToSlide(slide);
+      activateDot(slide);
+    }
+  });
 };
-prevDom.onclick = function () {
-  showSlider("prev");
-};
-let runTimeOut;
-let runNextAuto = setTimeout(() => {
-  next.click();
-}, timeAutoNext);
-function showSlider(type) {
-  let SliderItemsDom = SliderDom.querySelectorAll(".carousel .list .item");
-  let thumbnailItemsDom = document.querySelectorAll(
-    ".carousel .thumbnail .item"
-  );
-  if (type === "next") {
-    SliderDom.appendChild(SliderItemsDom[0]);
-    thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
-    carouselDom.classList.add("next");
-  } else {
-    SliderDom.prepend(SliderItemsDom[SliderItemsDom.length - 1]);
-    thumbnailBorderDom.prepend(thumbnailItemsDom[thumbnailItemsDom.length - 1]);
-    carouselDom.classList.add("prev");
-  }
-  clearTimeout(runTimeOut);
-  runTimeOut = setTimeout(() => {
-    carouselDom.classList.remove("next");
-    carouselDom.classList.remove("prev");
-  }, timeRunning);
-  clearTimeout(runNextAuto);
-  runNextAuto = setTimeout(() => {
-    next.click();
-  }, timeAutoNext);
-}
-// Sections title animations
-const allSections = document.querySelectorAll(".section");
-const revealSection = function (entries, observer) {
-  const [entry] = entries;
-  if (!entry.isIntersecting) return;
-  entry.target.classList.remove("section--hidden");
-  observer.unobserve(entry.target);
-};
-const sectionObserver = new IntersectionObserver(revealSection, {
-  root: null,
-  threshold: 0.15,
-});
-allSections.forEach(function (section) {
-  sectionObserver.observe(section);
-  section.classList.add("section--hidden");
-});
+slider();
 
 //carousel home slider end
 
@@ -78,6 +103,22 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 //babber slider end
+// Sections title animations
+const allSections = document.querySelectorAll(".section");
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove("section--hidden");
+  observer.unobserve(entry.target);
+};
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add("section--hidden");
+});
 
 // Toggle nav
 const toggleBtn = document.querySelector(".toggle_btn");
@@ -139,7 +180,7 @@ navbar.addEventListener("mouseout", handleHover.bind(1));
 //end fade
 
 // Sticky nav
-const carousel = document.querySelector(".carousel");
+const carousel = document.querySelector(".home-slider");
 const nav = document.querySelector(".nav");
 const navHeight = nav.getBoundingClientRect().height;
 const stickyNav = function (entries) {
